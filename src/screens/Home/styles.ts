dbd9283c1bf964d0496137d63842ab3572de434f
gustavo867 <tartatavo3@gmail.com>
2020-10-07 18:29:19 -0300

    line

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,10 +2,6 @@ import styled from "styled-components/native";
 
 import { Theme } from "../Login/styles";
 
-interface Props {
-  visible: boolean;
-}
-
 export const Container = styled.SafeAreaView`
   background-color: ${(props: Theme) => props.theme.background};
   flex: 1;
@@ -37,27 +33,4 @@ export const BetweenRow = styled.View`
 export const QrCodeButton = styled.TouchableOpacity`
   margin-right: 25px;
   margin-top: 5px;
-`;
-
-export const FiltersScroll = styled.ScrollView.attrs({
-  horizontal: true,
-})`
-  flex-grow: 0;
-  margin-top: ${(props: Props) => (props.visible ? 20 : 0)}px;
-`;
-
-export const FilterButton = styled.TouchableOpacity`
-  width: 100px;
-  height: 40px;
-  border-radius: 8px;
-  border-color: #ccc;
-  border-width: 1px;
-  margin: 15px;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const FilterText = styled.Text`
-  color: #fff;
-  opacity: 0.8;
 `;
